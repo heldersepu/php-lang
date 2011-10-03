d3c05e550d10ee723b8d045c6471be55b765bcc8
lekshmic@gmail.com <lekshmic@gmail.com@8e7a05c8-2747-43cc-2b89-0d474b88733f>
2011-10-03 14:08:03 +0000

    Added a new language file

---
+++ trunk/lang/lang.hi.php
@@ -0,0 +1,23 @@
+<?php
+  /*
+    NOTES:
+      Basic Notes:
+        Only change the RIGHT side text - not the left. NEVER EVER CHANGE THE LEFT.
+        NEVER change the name of the class...
+        Use spaces to indent the code; DO NOT USE TABs TO INDENT THE CODE!
+  */
+
+    class language {
+        public $hello                 = "Namaskar !!!";
+        public $photo_album           = "My Photo Album";
+        public $some_javascripts      = "Some JavaScripts";
+        public $my_resume             = "My Resume";
+        public $upload                = "Upload";
+        public $videos                = "Videos";
+        public $tic_tac_toe           = "Tic Tac Toe";
+        public $emulators             = "Emulators";
+        public $code_samples          = "Code Samples";
+        public $news_sites            = "News sites";
+        public $gmapcatcher           = "GMapCatcher";
+    }
+?>
